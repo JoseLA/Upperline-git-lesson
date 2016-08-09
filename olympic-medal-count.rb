92685164be8aeb421c541aaeb4b8621d8bd4079f
josela <joseloretoao@gmail.com>
2016-08-09 14:17:13 +0000

    changed olympic medal file

---
+++ olympic-medal-count.rb
@@ -0,0 +1,17 @@
+puts "U.S OLYMPIC MEDAL COUNT"
+
+num_gold = 5
+num_silver = 7
+num_bronze = 7
+
+total = num_gold + num_silver + num_bronze
+
+puts "the U.S has won:"
+puts "gold: #{num_gold}"
+puts "Silver #{num_silver}"
+puts "bronze: " + num_bronze.to_s
+
+puts "suimming"
+puts "volleyball"
+puts "air rifle".reverse
+puts "diving".gsub("i", "p")
